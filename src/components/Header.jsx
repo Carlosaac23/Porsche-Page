13ef64c999f21aabc76bf9d5cce6a999e614d7bc
Carlos <carlosaac232001@gmail.com>
2025-08-18 22:03:44 -0500

    feat(components): create first two components to the page

---
+++ src/components/Header.jsx
@@ -0,0 +1,32 @@
+export default function Header() {
+  return (
+    <div className='flex justify-between items-center p-7'>
+      <a className='flex items-center gap-3' href='#home'>
+        <img className='w-14' src='../../public/porsche.svg' alt='Logo' />
+        <span>Porsche</span>
+      </a>
+      <ul className='flex list-none gap-10 font-semibold text-lg'>
+        <li>
+          <a className='hover:text-neutral-300 duration-200 ease-in-out' href='#home'>
+            Home
+          </a>
+        </li>
+        <li>
+          <a className='hover:text-neutral-300 duration-200 ease-in-out' href='#about'>
+            About
+          </a>
+        </li>
+        <li>
+          <a className='hover:text-neutral-300 duration-200 ease-in-out' href='#popular'>
+            Popular
+          </a>
+        </li>
+        <li>
+          <a className='hover:text-neutral-300 duration-200 ease-in-out' href='#featured'>
+            Featured
+          </a>
+        </li>
+      </ul>
+    </div>
+  );
+}
---
+++ src/components/Home.jsx
@@ -0,0 +1,9 @@
+export default function Home() {
+  return (
+    <div className='flex flex-col items-center justify-center mt-10'>
+      <h1 className='text-4xl mb-2'>Choose the best car</h1>
+      <p className='text-base mb-8'>Porsche Taycan Turbo S</p>
+      <img className='w-96' src='../../public/home.webp' alt='Home Porsche image' />
+    </div>
+  );
+}
