819c5602ded6d1f0bc2f47238d021252aef7a6fd
Carlos <carlosaac232001@gmail.com>
2025-08-22 23:16:44 -0500

    feat(carsSwiper): add id to section and add all cards in swiper using props to dinamically add them

--- src/components/CarsSwiper/CarsSwiper.jsx
+++ src/components/CarsSwiper/CarsSwiper.jsx
@@ -4,7 +4,7 @@ import 'swiper/css';
 
 export default function CarsSwiper() {
   return (
-    <section className='p-10'>
+    <section id='popular' className='p-10'>
       <Swiper
         spaceBetween={60}
         slidesPerView={3}
@@ -18,22 +18,19 @@ export default function CarsSwiper() {
         }}
       >
         <SwiperSlide>
-          <Card />
+          <Card brand='Porsche' model='911 Turbo S' img='/popular1.webp' acceleration='2,7' topSpeed='330' fuelGas='66.6' price='373,000' />
         </SwiperSlide>
         <SwiperSlide>
-          <Card />
+          <Card brand='Porsche' model='911 Carrera 4 GTS' img='/popular2.webp' acceleration='3,0' topSpeed='312' fuelGas='66.6' price='265,700' />
         </SwiperSlide>
         <SwiperSlide>
-          <Card />
+          <Card brand='Porsche' model='Panamera Turbo S E-Hybrid' img='/popular3.webp' acceleration='2,9' topSpeed='325' fuelGas='79.8' price='265,700' />
         </SwiperSlide>
         <SwiperSlide>
-          <Card />
+          <Card brand='Porsche' model='Cayenne Turbo GT' img='/popular4.webp' acceleration='3,3' topSpeed='305' fuelGas='89.7' price='303,700' />
         </SwiperSlide>
         <SwiperSlide>
-          <Card />
-        </SwiperSlide>
-        <SwiperSlide>
-          <Card />
+          <Card brand='Porsche' model='911 GT3' img='/popular5.webp' acceleration='3,4' topSpeed='318' fuelGas='64' price='300,400' />
         </SwiperSlide>
       </Swiper>
     </section>
