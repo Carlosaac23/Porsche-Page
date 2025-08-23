247502c039ad154b56e9c8cf4187e7cfbc6604d3
Carlos <carlosaac232001@gmail.com>
2025-08-23 10:39:23 -0500

    style(card): add margin-bottom to separate bullets pagination and change price font size

--- src/components/CarsSwiper/Card.jsx
+++ src/components/CarsSwiper/Card.jsx
@@ -4,13 +4,13 @@ import PropTypes from 'prop-types';
 export default function Card({ brand, model, img, acceleration, topSpeed, fuelGas, price }) {
   return (
     <div
-      className='flex flex-col items-center p-5 border border-neutral-800 rounded-lg hover:border-neutral-600 duration-200 ease-out cursor-pointer
+      className='max-w-2xs flex flex-col items-center p-5 mb-16 border border-neutral-800 rounded-lg hover:border-neutral-600 duration-200 ease-out cursor-pointer
     '
     >
       <h2 className='w-full text-left font-bold text-lg'>{brand}</h2>
       <h3 className='w-full text-left font-light text-xs text-neutral-400'>{model}</h3>
       <img className='mb-4 hover:translate-x-1.5 duration-200 ease-out' src={img} alt='Imagen del auto' />
-      <ul className='flex justify-between gap-3 mb-2'>
+      <ul className='w-full flex justify-start gap-3 mb-2'>
         <li className='flex items-center gap-1'>
           <span className='text-xs'>{acceleration} s</span>
           <ChevronsUp size={14} color='#fafafa' />
@@ -24,7 +24,7 @@ export default function Card({ brand, model, img, acceleration, topSpeed, fuelGa
           <Fuel size={14} color='#fafafa' />
         </li>
       </ul>
-      <p className='w-full text-left font-bold'>USD {price}</p>
+      <p className='w-full text-left text-sm font-bold'>USD {price}</p>
     </div>
   );
 }
