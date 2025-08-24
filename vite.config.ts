71001e2f025bc67c879d3cb7f7c4c37c3777bcc7
Carlos <carlosaac232001@gmail.com>
2025-08-24 13:40:34 -0500

    chore: update vite.config file to ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,8 @@
-import { defineConfig } from 'vite';
+import { defineConfig, UserConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 import tailwindcss from '@tailwindcss/vite';
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss()],
-});
+} satisfies UserConfig);
