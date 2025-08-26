a50300945f4e1a181951118ea4df62ecbcd2fa7e
Carlos <carlosaac232001@gmail.com>
2025-08-26 12:36:09 -0500

    style(home): remove all Motion animations

--- src/components/Welcome/Home.tsx
+++ src/components/Welcome/Home.tsx
@@ -1,4 +1,3 @@
-import { motion } from 'motion/react';
 import { Fuel, ChevronsUp, Gauge, Power, ArrowBigUpDash } from 'lucide-react';
 import { useState } from 'react';
 import useSound from 'use-sound';
@@ -20,76 +19,35 @@ export default function Home() {
 
   return (
     <section className='flex flex-col items-center justify-center mt-10'>
-      <motion.h1
-        className='text-2xl md:text-3xl xl:text-4xl mb-2 font-bold'
-        initial={{ opacity: 0, x: 50 }}
-        animate={{ opacity: 1, x: 0 }}
-        transition={{ duration: 1, ease: 'easeOut' }}
-      >
-        Choose the best car
-      </motion.h1>
-      <motion.p
-        className='text-sm xl:text-lg mb-2 text-neutral-400'
-        initial={{ opacity: 0, x: 50 }}
-        animate={{ opacity: 1, x: 0 }}
-        transition={{ duration: 1, ease: 'easeOut' }}
-      >
-        Porsche 911 Turbo S
-      </motion.p>
-      <motion.div
-        className='flex items-center gap-2 mb-10'
-        initial={{ opacity: 0, x: -50 }}
-        animate={{ opacity: 1, x: 0 }}
-        transition={{ duration: 1, ease: 'easeOut' }}
-      >
+      <h1 className='text-2xl md:text-3xl xl:text-4xl mb-2 font-bold'>Choose the best car</h1>
+      <p className='text-sm xl:text-lg mb-2 text-neutral-400'>Porsche 911 Turbo S</p>
+      <div className='flex items-center gap-2 mb-10'>
         <Fuel className='xl:w-5 xl:h-5' size={16} color='#fafafa' />
         <p className='font-semibold text-sm xl:text-md'>Engine Car</p>
-      </motion.div>
+      </div>
 
-      <motion.img
-        className='w-full xl:w-3xl mb-6'
-        src='/home.png'
-        alt='Home Porsche image'
-        initial={{ opacity: 0, x: -50 }}
-        animate={{ opacity: 1, x: 0 }}
-        transition={{ duration: 1, ease: 'easeOut' }}
-      />
+      <img className='w-full xl:w-3xl mb-6' src='/home.png' alt='Home Porsche image' />
 
       <div onClick={handleClick} className='p-4 mb-4 rounded-full hover:bg-neutral-700/15 duration-250 ease-out cursor-pointer'>
         <Power color='#fafafa' />
       </div>
 
       <ul className='w-full flex items-center justify-evenly mt-5'>
-        <motion.li
-          className='flex flex-col items-center gap-1 w-20'
-          initial={{ opacity: 0, y: 50 }}
-          animate={{ opacity: 1, y: 0 }}
-          transition={{ duration: 1, ease: 'easeOut' }}
-        >
+        <li className='flex flex-col items-center gap-1 w-20'>
           <ChevronsUp size={20} color='#fafafa' />
           <span className='text-xs'>2,7 s</span>
           <p className='text-sm font-extralight'>Acceleration</p>
-        </motion.li>
-        <motion.li
-          className='flex flex-col items-center gap-1 w-20'
-          initial={{ opacity: 0, y: 50 }}
-          animate={{ opacity: 1, y: 0 }}
-          transition={{ duration: 1, ease: 'easeOut' }}
-        >
+        </li>
+        <li className='flex flex-col items-center gap-1 w-20'>
           <Gauge size={20} color='#fafafa' />
           <span className='text-xs'>330 km/h</span>
           <p className='text-sm font-extralight'>Top Speed</p>
-        </motion.li>
-        <motion.li
-          className='flex flex-col items-center gap-1 w-20'
-          initial={{ opacity: 0, y: 50 }}
-          animate={{ opacity: 1, y: 0 }}
-          transition={{ duration: 1, ease: 'easeOut' }}
-        >
+        </li>
+        <li className='flex flex-col items-center gap-1 w-20'>
           <ArrowBigUpDash size={20} color='#fafafa' />
           <span className='text-xs'>650 hp</span>
           <p className='text-sm font-extralight'>Power</p>
-        </motion.li>
+        </li>
       </ul>
     </section>
   );
