7c82e6dfbd5106c4aa3f7906b302a2c69f36481d
Carlos <carlosaac232001@gmail.com>
2025-08-26 12:35:30 -0500

    style(about): remove all Motion animations

--- src/components/About/About.tsx
+++ src/components/About/About.tsx
@@ -1,29 +1,15 @@
-import { motion } from 'motion/react';
-
 export default function About() {
   return (
     <section className='my-30 md:flex justify-around gap-14' id='about'>
       <div className='relative w-fit mb-20'>
-        <motion.img
-          initial={{ scale: 0.7, opacity: 0 }}
-          animate={{ scale: 1, opacity: 1 }}
-          transition={{ duration: 1, ease: 'easeOut' }}
-          className='w-full 2xl:w-2xl h-auto object-cover rounded-lg'
-          src='/interior.webp'
-          alt='Interior Porsche Image'
-        />
+        <img className='w-full 2xl:w-2xl h-auto object-cover rounded-lg' src='/interior.webp' alt='Interior Porsche Image' />
 
-        <motion.div
-          initial={{ opacity: 0, x: 50 }}
-          animate={{ opacity: 1, x: 0 }}
-          transition={{ duration: 1, ease: 'easeOut' }}
-          className='absolute -right-3 -bottom-5 md:-right-6 md:-bottom-6 p-4 backdrop-blur-xs bg-neutral-500/10 rounded-lg text-center'
-        >
+        <div className='absolute -right-3 -bottom-5 md:-right-6 md:-bottom-6 p-4 backdrop-blur-xs bg-neutral-500/10 rounded-lg text-center'>
           <h3 className='font-bold'>1110 Nm</h3>
           <p className='text-sm font-light'>Up to 1110 Nm</p>
-        </motion.div>
+        </div>
       </div>
-      <motion.div initial={{ opacity: 0, x: -50 }} animate={{ opacity: 1, x: 0 }} transition={{ duration: 1, ease: 'easeOut' }} className='md:w-lg'>
+      <div className='md:w-lg'>
         <h2 className='text-lg font-bold mb-6 md:text-xl lg:text-2xl'>Overfeel</h2>
         <p className='text-sm text-neutral-400 font-light text-balance mb-10 md:text-md lg:text-base'>
           The overwhelming thrill of standing before the Porsche 911 Turbo S: a masterpiece where engineering meets pure adrenaline. With breathtaking
@@ -33,7 +19,7 @@ export default function About() {
         <a href='#' className='font-semibold text-sm border border-neutral-800 py-4 px-7 rounded-lg hover:border-neutral-600'>
           More information
         </a>
-      </motion.div>
+      </div>
     </section>
   );
 }
