b4274c17818aee4a2981a12f4a1b6bad79f8c8e2
Carlos <carlosaac232001@gmail.com>
2025-08-28 11:34:21 -0500

    feat(main): import pages to router them with react-router

--- src/main.tsx
+++ src/main.tsx
@@ -1,12 +1,21 @@
+import { createBrowserRouter, RouterProvider } from 'react-router-dom';
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
+import AboutPage from './pages/AboutPage';
+import NotFoundPage from './pages/NotFoundPage';
 import './styles/global.css';
+
+const router = createBrowserRouter([
+  { path: '/', element: <App /> },
+  { path: '/about', element: <AboutPage /> },
+  { path: '*', element: <NotFoundPage /> },
+]);
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
     <main className='px-8 sm:px-12 md:px-20 lg:px-22 xl:px-28 2xl:px-40'>
-      <App />
+      <RouterProvider router={router} />
     </main>
   </React.StrictMode>
 );
