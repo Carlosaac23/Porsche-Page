f6602546e61ac28a397bae71b59f7d9f4bfa091c
Carlos <carlosaac232001@gmail.com>
2025-08-28 22:53:24 -0500

    chore: add Footer component

--- src/pages/AboutPage.tsx
+++ src/pages/AboutPage.tsx
@@ -1,3 +1,4 @@
+import Footer from '../components/Footer/Footer';
 import Header from '../components/Welcome/Header';
 
 export default function AboutPage() {
@@ -7,6 +8,9 @@ export default function AboutPage() {
       <section>
         <h1 className='text-center text-4xl'>About Page</h1>
       </section>
+      <div className='fixed bottom-0 left-0 right-0'>
+        <Footer />
+      </div>
     </>
   );
 }
