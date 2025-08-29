9b7060be8fb203a97ae8a33bc19c36bd66a40ff6
Carlos <carlosaac232001@gmail.com>
2025-08-28 22:54:31 -0500

    feat: add Offers and Footer component

--- src/pages/MainPage.tsx
+++ src/pages/MainPage.tsx
@@ -3,15 +3,21 @@ import About from '../components/About/About';
 import CarsSwiper from '../components/CarsSwiper/CarsSwiper';
 import Features from '../components/Features/Features';
 import Home from '../components/Welcome/Home';
+import ScrollToTopButton from '../components/ScrollUp';
+import Offers from '../components/Subscribe/Offers';
+import Footer from '../components/Footer/Footer';
 
 export default function MainPage() {
   return (
     <>
+      <ScrollToTopButton />
       <Header />
       <Home />
       <About />
       <CarsSwiper />
       <Features />
+      <Offers />
+      <Footer />
     </>
   );
 }
