b834926eb6496d2407593643551389844259b556
Carlos <carlosaac232001@gmail.com>
2025-09-01 23:06:14 -0500

    chore: add updated import from component with new name

--- src/pages/MainPage.tsx
+++ src/pages/MainPage.tsx
@@ -4,7 +4,7 @@ import CarsSwiper from '@/components/CarsSwiper/CarsSwiper';
 import Features from '@/components/Features/Features';
 import Home from '@/components/Welcome/Home';
 import ScrollToTopButton from '@/components/ScrollUp';
-import Offers from '@/components/Subscribe/Offers';
+import Offers from '@/components/Subscribe/Subscribe';
 import Footer from '@/components/Footer/Footer';
 
 export default function MainPage() {
