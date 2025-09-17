03d2674bfe90f4c03670b78da26c55bf4d3090a4
Carlos <carlosaac232001@gmail.com>
2025-09-17 16:43:16 -0500

    chore: delete tailwind.config.ts file due is not mandatory

--- tailwind.config.ts
+++
@@ -1,9 +0,0 @@
-import type { Config } from 'tailwindcss';
-
-export default {
-  content: ['./index.html', './src/**/*.{js,jsx,ts,tsx,html}'],
-  theme: {
-    extend: {},
-  },
-  plugins: [],
-} satisfies Config;
