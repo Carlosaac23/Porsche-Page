3f7beb9a482f1b45a6f7b1e0097d6c6fb02b7b48
Carlos <carlosaac232001@gmail.com>
2025-09-17 16:46:10 -0500

    chore: import all components here instead of import MainPage since it was delete

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,23 @@
-import MainPage from './pages/MainPage';
+import Header from '@/components/Header';
+import About from '@/components/About';
+import CarsSwiper from '@/components/CarSwiper';
+import Features from '@/components/Features';
+import Home from '@/components/Home';
+import ScrollTopButton from '@/components/ScrollUp';
+import Offers from '@/components/Subscribe';
+import Footer from '@/components/Footer';
 
 export default function App() {
   return (
     <>
-      <MainPage />
+      <ScrollTopButton />
+      <Header />
+      <Home />
+      <About />
+      <CarsSwiper />
+      <Features />
+      <Offers />
+      <Footer />
     </>
   );
 }
