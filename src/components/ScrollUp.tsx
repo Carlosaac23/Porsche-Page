0a2e4a4fbbd9ba1b17f499b5d00fea565b23d3fd
Carlos <carlosaac232001@gmail.com>
2025-09-17 16:46:56 -0500

    chore: change component name

--- src/components/ScrollUp.tsx
+++ src/components/ScrollUp.tsx
@@ -1,7 +1,7 @@
 import { useEffect, useState } from 'react';
 import { ArrowUp } from 'lucide-react';
 
-export default function ScrollToTopButton() {
+export default function ScrollTopButton() {
   const [visible, setVisible] = useState(false);
 
   useEffect(() => {
